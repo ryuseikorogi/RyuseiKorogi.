e92bd08dc8d20e0dea4531e11fcc93836b3a314a
your-name <your-email>
2020-11-23 18:58:24 +0900

    htmlの編集
    about・skills・works・contactsの追加と@sites.eachでsiteのカラムデータを全て取得できるようにした。

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  get '/static_pages/index',to:"static_pages_controller#index"
   get 'static_pages/help'
-    get 'posts', to:'posts#index'
+    root to: "static_pages#index"
 end
