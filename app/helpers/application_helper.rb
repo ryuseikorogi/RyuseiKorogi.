806fc53109b8a891b5934a59a463688af8f65ffe
ryuseikorogi <ryuseikorogi@gmail.com>
2021-01-31 16:40:06 +0900

    viewsの写真の遅延読み込み設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def lazysizes_image_tag(source, options={})
+    options['data-src'] = source
+    if options[:class].blank?
+      options[:class] = "lazyload"
+    else
+      options[:class] = "lazyload #{options[:class]}"
+    end
+     image_tag("画像を読み込んでない時の軽い画像", options) + ("<noscript>#{image_tag(source, options)}</noscript>").html_safe
+  end
 end
